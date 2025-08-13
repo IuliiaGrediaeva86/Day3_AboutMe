2ea3a333261d599ebb1a59900070fa2af6053a00
IuliiaGrediaeva86 <igrediaeva@gmail.com>
2025-08-12 21:30:12 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "Yulia";
+        int age = 29;
+        float height = 1.58f;
+        char gender = 'F';
+
+        Console.WriteLine("Write name: ");
+        name = Console.ReadLine();
+
+        Console.WriteLine("Your age: ");
+        age = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Height: ");
+        height = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Gender: ");
+        gender = Console.ReadLine()[0];
+
+        int days = age * 365;
+        Console.WriteLine(name + " " + age + "y.o. " + height + " cm " + " " + gender + " " + days + " days you have lived. ");
+
+ 
+    
+
+    }
+}
